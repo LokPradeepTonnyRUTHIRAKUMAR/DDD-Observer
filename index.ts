dd9f5abfea7bc5443f2a80a26d3599197cee77b7
Aleksandra Temereva <aleksandra.temereva@epita.fr>
2026-03-09 11:50:53 +0100

    added a try catch block

--- index.ts
+++ index.ts
@@ -4,17 +4,21 @@ import { loggerObserver } from "./infrastructure/observers/observer.ts";
 import { emailObserver } from "./infrastructure/observers/email.ts";
 import { portfolioObserver } from "./infrastructure/observers/database.ts";
 
-let project = createProject("Yann", "yan@mail.com", 1000, true);
+try {
+  let project = createProject("Yann", "yan@mail.com", 1000, true);
 
-project = subscribe(project, loggerObserver);
-project = subscribe(project, emailObserver);
-project = subscribe(project, portfolioObserver);
+  project = subscribe(project, loggerObserver);
+  project = subscribe(project, emailObserver);
+  project = subscribe(project, portfolioObserver);
 
-project = {
-  ...project,
-  status: "Build",
-};
+  project = {
+    ...project,
+    status: "Build",
+  };
 
-project = deployProject(project);
+  project = deployProject(project);
 
-console.log("Final project status:", project.status);
+  console.log("Final project status:", project.status);
+} catch (error) {
+  console.error("Error:", error.message);
+}
